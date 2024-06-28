e9e61f1ef458de5a7c8c1427816eb77f0a13900d
NxtPerfect <alakaxan@protonmail.com>
2024-06-28 20:00:49 +0200

    Added appropriate pages
    started working on categories page

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -22,8 +22,11 @@
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
 				<a href="/about">About</a>
 			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
+			<li aria-current={$page.url.pathname.startsWith('/categories') ? 'page' : undefined}>
+				<a href="/categories">Categories</a>
+			</li>
+			<li aria-current={$page.url.pathname.startsWith('/newest') ? 'page' : undefined}>
+				<a href="/newest">Newest</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
@@ -32,7 +35,7 @@
 	</nav>
 
 	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
+		<a href="https://github.com/nxtperfect">
 			<img src={github} alt="GitHub" />
 		</a>
 	</div>
